036d619656ae01c6eb45a60eb8948f93d7d6ebac
HoilJeong <93916454+HoilJeong@users.noreply.github.com>
2022-08-17 18:05:35 +0900

    초기 커밋.

---
+++ c_study/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+ * Name: __01_Array_Init
+ * DESC: 일차원 배열 기본
+ */
+ 
+namespace _01_Array_Init
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrNum = new int[3];
+
+            arrNum[0] = 1;
+            arrNum[1] = 2;
+            arrNum[2] = 3;
+
+            for (int i = 0; i < arrNum.Length; i++)
+            {
+                Console.Write("{0}", arrNum[i]);
+            }
+
+            Console.WriteLine("\n arrNum.GetType(): {0}", arrNum.GetType());
+        }
+    }
+}
