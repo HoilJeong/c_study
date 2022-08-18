abc0c91ec707e6c81ef13310d60600738b8c0d99
HoilJeong <93916454+HoilJeong@users.noreply.github.com>
2022-08-18 14:17:36 +0900

    다차원 배열 기본

---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 2;
+            int c;
+          
+            Console.WriteLine(a + b);
+        }
+    }
+}
