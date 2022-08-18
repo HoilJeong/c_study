974184cd0e5bdb7c6d39ea7bcf2d47aae952a7c9
HoilJeong <93916454+HoilJeong@users.noreply.github.com>
2022-08-18 14:25:33 +0900

    다차원 배열 다양한 선언 및 초기화 방법

--- 04_Array_Multi2/Properties/AssemblyInfo.cs
+++ 04_Array_Multi2/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("bf17ca19-e193-4723-8bf7-24433d8411eb")]
+[assembly: Guid("021db510-62d1-433e-aace-d1e93de5e905")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
