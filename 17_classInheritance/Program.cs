31883a0228bb8afdb687c047c72753bb43a346d1
HoilJeong <93916454+HoilJeong@users.noreply.github.com>
2022-08-20 18:27:32 +0900

    클래스 상속 기초

---
+++ 17_classInheritance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: _17_classInheritance
+ * DESC: 클래스 상속 기초
+ */
+
+namespace _17_classInheritance
+{
+    class Program
+    {
+        class Super
+        {
+            protected int a;
+
+            public void Print()
+            {
+                Console.WriteLine("Super Print()");
+            }
+        }
+
+        class Sub : Super
+        {
+            int b;
+
+            public void Print()
+            {
+                Console.WriteLine($"Sub Print() a: " + a + " b: " + b);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Sub sub = new Sub();
+            sub.Print();
+        }
+    }
+}
---
+++ 19_class_base/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: _19_class_base
+ * DESC: base 키워드
+ */
+
+namespace _19_class_base
+{
+    class Super
+    {
+        int num;
+        protected string name;
+
+        public Super(int num)
+        {
+            this.num = num;
+        }
+
+        public void PrintSuper()
+        {
+            Console.WriteLine($"Super num: " + num);
+        }
+    }
+
+    class Sub : Super
+    {
+        string name;
+        public Sub(int num, string name) : base(num)
+        {
+            this.name = name;
+        }
+
+        public void PrintSub()
+        {
+            base.name = "Super";
+            base.PrintSuper();
+
+            Console.WriteLine($"Super name: " + base.name);
+            Console.WriteLine($"Sub name: " + this.name);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sub sub = new Sub(26, "Jack");
+            sub.PrintSub();
+        }
+    }
+}
