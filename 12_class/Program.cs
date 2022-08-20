dc7fa0635867d148ed4e754809ac9b50a3bf3a21
HoilJeong <93916454+HoilJeong@users.noreply.github.com>
2022-08-20 15:32:32 +0900

    클래스 기본

---
+++ 12_class/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: _12_class
+ * DESC: 클래스 기본
+ */
+
+namespace _12_class
+{
+    class AA
+    {
+        // 멤버 변수
+        int num1;
+        public int num2, num3;
+
+        // 멤버 함수
+        public void Print()
+        {
+            Console.WriteLine($"aa: " + num1 + " bb: " + num2 + " cc: " + num3);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AA aa = new AA();
+            aa.Print();
+        }
+    }
+}
