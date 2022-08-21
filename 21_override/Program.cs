008689f2b05dc6aad19c2b7b1c190f98e443fd4b
HoilJeong <93916454+HoilJeong@users.noreply.github.com>
2022-08-21 17:22:32 +0900

    오버라이딩(다형성)

---
+++ 21_override/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: _21_override
+ * DESC: 오버라이딩(다형성):
+ * 같은 기능을 하는 함수를 오버라이딩해서 다른 기능을 추가적으로 할 수 있도록 하는 기능
+ */
+
+namespace _21_override
+{
+    class Super
+    {
+        protected int num;
+
+        public virtual void Print()
+        {
+            Console.WriteLine($"num: " + num);
+        }
+    }
+
+    class AA : Super
+    {
+        public int a;
+
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"AA a: " + a);
+        }
+    }
+
+    class BB : Super
+    {
+        public int b;
+
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"BB b: " + b);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Super super = new Super();
+            super.Print();
+
+            Super aa = new AA();
+            aa.Print();
+
+            Super bb = new BB();
+            bb.Print();
+        }
+    }
+}
---
+++ 22_override2/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: _22_override2
+ * DESC: 오버라이딩(다형성): 실제 게임에서 사용되는 예를 보여준다.
+ */
+
+namespace _22_override2
+{
+    class Program
+    {
+        class Army
+        {
+            protected int _HP;
+            protected int _MP;
+            protected int _Speed;
+            protected int _Attack;
+
+            public virtual void Run()
+            {
+                Console.Write($"" + _Speed + "의 속도로 ");
+            }
+
+            public virtual void Attack()
+            {
+                Console.WriteLine();
+
+                if (this is Healer)
+                {
+                    Console.Write($"[마법 공격력 - " + (_Attack * _MP) + "] 으로 ");
+                }
+                else
+                {
+                    Console.Write($"[공격력 - " + _Attack + "]으로 ");
+                }
+            }
+        }
+
+        class Barbarian : Army
+        {
+            public Barbarian()
+            {
+                _HP = 100;
+                _MP = 0;
+                _Speed = 100;
+                _Attack = 100;
+            }
+
+            public override void Run()
+            {
+                base.Run();
+                Console.WriteLine("Barbarian 달려갑니다.");
+            }
+
+            public override void Attack()
+            {
+                base.Attack();
+                Console.WriteLine("Barbarian 공격!!!");
+            }
+        }
+
+        class Giant : Army
+        {
+            public Giant()
+            {
+                _HP = 200;
+                _MP = 0;
+                _Speed = 10;
+                _Attack = 200;
+            }
+
+            public override void Run()
+            {
+                base.Run();
+                Console.WriteLine("Giant 달려갑니다.");
+            }
+
+            public override void Attack()
+            {
+                base.Attack();
+                Console.WriteLine("Giant 공격!!!");
+            }
+        }
+
+        class Healer : Army
+        {
+            public Healer()
+            {
+                _HP = 50;
+                _MP = 100;
+                _Speed = 200;
+                _Attack = 10;
+            }
+
+            public override void Run()
+            {
+                base.Run();
+                Console.WriteLine($"Healer 날아갑니다." + _Speed);
+            }
+
+            public override void Attack()
+            {
+                base.Attack();
+                Console.WriteLine("Healer 마법 공격!!!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Army[] arrArmys = new Army[10];
+
+            arrArmys[0] = new Barbarian();
+            arrArmys[1] = new Giant();
+            arrArmys[2] = new Healer();
+
+            for (int i = 0; i < arrArmys.Length; i++)
+            {
+                if (null != arrArmys[i])
+                {
+                    arrArmys[i].Run();
+                }
+            }
+
+            for (int i = 0; i < arrArmys.Length; i++)
+            {
+                if (null != arrArmys[i])
+                {
+                    arrArmys[i].Attack();
+                }
+            }
+        }
+    }
+}
