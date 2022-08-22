82d53b445cdd5e7142f27143f88e40dac9b36514
HoilJeong <93916454+HoilJeong@users.noreply.github.com>
2022-08-22 15:49:00 +0900

    abstract 기본

---
+++ 27_abstract/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: _27_abstract
+ * DESC: abstract 기본
+ */
+
+namespace _27_abstract
+{
+    abstract class abstractAA
+    {
+        public int num;
+
+        public abstract void AbstractPrint(); // 강제적으로 상속받은 클래스에서 구현부를 정의
+
+        public virtual void VirtualPrint()
+        {
+
+        }
+
+        public void Print()
+        {
+
+        }
+    }
+
+    class AA : abstractAA
+    {
+        public override void AbstractPrint()
+        {
+            Console.WriteLine("abstractPrint");
+        }
+
+        public override void VirtualPrint()
+        {
+            base.VirtualPrint();
+            Console.WriteLine("override void virtualPrint()");
+        }
+    }
+
+    class Program
+    {             
+        static void Main(string[] args)
+        {
+            AA aa = new AA();
+            aa.AbstractPrint();
+            aa.VirtualPrint();          
+        }
+    }
+}
