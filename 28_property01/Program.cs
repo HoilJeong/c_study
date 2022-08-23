1859129e09d1adc31e484f8e138cdc3bf3df1839
HoilJeong <93916454+HoilJeong@users.noreply.github.com>
2022-08-23 14:35:58 +0900

    get, set 키워드 구현

---
+++ 28_property01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: _28_property01
+ * DESC: get, set 키워드 구현
+ */
+
+namespace _28_property01
+{
+    class AA
+    {
+        private int num;
+
+        public int NUM
+        {
+            get { return num; }
+            set { num = value; } // set부분을 구현하지 않으면 readonly와 같은 읽기 전용
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AA aa = new AA();
+            aa.NUM = 100;
+
+            Console.WriteLine("aa.NUM: " + aa.NUM);
+        }
+    }
+}
---
+++ 29_property02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: _28_property01
+ * DESC: get, set 키워드 구현 - 자동 구현 프로퍼티
+ */
+
+namespace _29_property02
+{
+    class AA
+    {
+        private int num;
+        private string name;
+
+        public int NUM { get; set; }
+        public string NAME { get; set; } = "NoName";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AA aa = new AA();
+            Console.WriteLine("aa.num: " + aa.NUM);
+            Console.WriteLine("aa.NAME: " + aa.NAME);
+
+            aa.NUM = 100;
+            Console.WriteLine("aa.num: " + aa.NUM);
+            aa.NAME = "Jack";
+            Console.WriteLine("aa.NAME: " + aa.NAME);
+        }
+    }
+}
