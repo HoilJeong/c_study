90793e2120de5d7cb42c147a58fb8a3af242c36b
HoilJeong <93916454+HoilJeong@users.noreply.github.com>
2022-08-24 21:04:25 +0900

    ArrayList 기본

---
+++ 33_ArrayList/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: _33_ArrayList
+ * DESC: ArrayList 기본
+ */
+
+namespace _33_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrList = new ArrayList();
+
+            arrList.Add("Hello");
+            arrList.Add(10f);
+
+            for (int i = 0; i < 10; i++)
+            {
+                arrList.Add(i);
+            }
+
+            foreach (object data in arrList)
+            {
+                Console.WriteLine("arrList data: " + data);
+            }
+
+            // 배열 데이터로 초기화
+            Console.WriteLine("배열 데이터로 초기화");
+            int[] arrData = { 100, 200, 300 };
+            ArrayList arrCopyList = new ArrayList(arrData);
+
+            foreach (object data in arrCopyList)
+            {
+                Console.WriteLine("arrarrCopyList data: " + data);
+            }
+        }
+    }
+}
